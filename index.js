1fd13827ccb73114abfa3329a6cc6e9767550bb5
Comfort Ige <eligenius@gmail.com>
2023-06-23 08:32:14 +0100

    Get and display form content using FormData

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ closeModal.addEventListener("click", () => {
 
 consentForm.addEventListener("submit", (e) => {
   e.preventDefault()
+  const consentFormData = new FormData(consentForm)
+  const name = consentFormData.get("fullName")
+  const email = consentFormData.get("email")
+
   modalText.innerHTML = `
     <div class="modal-inner-loading">
       <img src="images/loading.svg" class="loading">
@@ -31,7 +35,7 @@ consentForm.addEventListener("submit", (e) => {
 
   setTimeout(() => {
     document.getElementById("modal-inner").innerHTML = `
-      <h2>Thanks you sucker! </h2>
+      <h2>Thanks <span class="modal-display-name">${name}</span>, you sucker! </h2>
       <p>We just sold the rights to your eternal soul.</p>
       <div class="idiot-gif">
           <img src="images/pirate.gif">
