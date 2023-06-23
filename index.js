565267f70ddaaf54b6cd25ed76cfcec169b70211
Comfort Ige <eligenius@gmail.com>
2023-06-23 07:00:33 +0100

    Add eventListener to accept button

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const modal = document.getElementById("modal")
 const closeModal = document.getElementById("modal-close-btn")
 const consentForm = document.getElementById("consent-form")
+const modalText = document.getElementById("modal-text")
+
 
 setTimeout(() => {
   modal.style.display = "inline"
@@ -12,4 +14,13 @@ closeModal.addEventListener("click", () => {
 
 consentForm.addEventListener("submit", (e) => {
   e.preventDefault()
+  modalText.innerHTML = `
+    <div class="modal-inner-loading">
+      <img src="images/loading.svg" class="loading">
+      <p id="uploadText">
+      Uploading your data to the dark web...
+      </p>
+    </div>
+  `
 })
+
